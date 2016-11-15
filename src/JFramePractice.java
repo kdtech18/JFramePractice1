684999ee78f5f1f0ce51d480a48a018e91345cea
Kenton Duprey <kdtech18@gmail.com>
2016-11-15 11:47:06 -0500

    adding comments and changing imports

--- src/JFramePractice.java
+++ src/JFramePractice.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.FlowLayout;
 
 public class JFramePractice
     {
@@ -16,17 +17,31 @@ public class JFramePractice
         JFrame aFrame = new JFrame("This is my first frame.");
         JLabel alabel = new JLabel("Kenton Duprey");
         JButton pressBtn = new JButton("Press Me");
-        
+        FlowLayout flow = new FlowLayout();
         
         // setSize(width, height)
         aFrame.setSize(450, 450);
+        
         // set title
         aFrame.setTitle("This is a set title");
+        
+        // set layout
+        aFrame.setLayout(flow);
+        
         // set if resizable
         aFrame.setResizable(false);
+        
+        // adds label
         aFrame.add(alabel);
+        
+        // adds button
         aFrame.add(pressBtn);
+        
+        // sets default close operation
         aFrame.setDefaultCloseOperation(aFrame.EXIT_ON_CLOSE);
+        
+        // sets to visible
         aFrame.setVisible(true);
+        
         }// end main method
     }
